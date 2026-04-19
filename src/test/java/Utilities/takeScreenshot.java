068d3676245c2f4d7751533ff7835f4a8a4eef42
Abhilash Katturoj <abhilashkatturoj09@gmail.com>
2026-04-19 22:23:38 +0530

    added 50% screenshot code.

---
+++ src/test/java/Utilities/takeScreenshot.java
@@ -0,0 +1,26 @@
+package Utilities;
+
+import RunTime.SharedDriver;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class takeScreenshot extends SharedDriver {
+
+    //    public  static sspath=take
+    public String getScreenShot(String testcasename) throws IOException {
+        System.out.println("in before screenshot");
+        WebDriver driver1 = getDriver();
+        TakesScreenshot ts = (TakesScreenshot) driver1;
+        File source = ts.getScreenshotAs(OutputType.FILE);
+        File file = new File("/Users/abhilashkatturoj/Abhilash/UIAutomationTestNG/imagereports/" + testcasename + ".png");
+        FileUtils.copyFile(source, file);
+        System.out.println("in after screenshot");
+        return "/Users/abhilashkatturoj/Abhilash/UIAutomationTestNG/imagereports/" + testcasename + ".png";
+
+    }
+}
